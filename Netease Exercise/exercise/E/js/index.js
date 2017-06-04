6d2e636ca7c869479a2ef4d1ee3abf9ac817993a
Eric Jiang <deviljiang021@gmail.com>
2017-06-04 10:32:03 +0800

    6_3

--- Netease Exercise/exercise/E/js/index.js
+++ Netease Exercise/exercise/E/js/index.js
@@ -32,8 +32,6 @@ document.getElementById('login_close').addEventListener('click', function(event)
 (function() {
     // 点击关注判断是否登录 弹出登录or设置关注
     follow.addEventListener('click', function(event) {
-        // alert('1');
-        // console.log(getCookie().followSuc);
         if (cookie_status.loginSuc == undefined) {
             login.classList.remove('off');
         } else if (cookie_status.loginSuc == "true") {
@@ -93,27 +91,12 @@ var uPwd = document.getElementById('pwd-err');
 var logBtn = document.getElementById('logBtn');
 
 form.addEventListener('submit', function(event) {
-        ajax({
-        url: "http://study.163.com/webDev/login.htm", //请求地址
-        type: "get", //请求方式
-        data: {
-            userName: "95b9941b277caf1c77ee35fee66fc5f6",
-            password: "a972aec008fd064f00ae77c3a6472cc2"
-        }, //请求参数
-        // dataType: "json",
-        success: function(res, xml) {
-            console.log(res);
-
-        },
-        fail: function(status) {
-            console.log('someThing was wrong: ' + status);
-        }
-
-    });
+_login();
 });
 // });
 
 
+// 登录
 function _login() {
     ajax({
         url: "http://study.163.com/webDev/login.htm", //请求地址
@@ -138,30 +121,27 @@ function _login() {
 
 
 
+// 获取课程
+(function() {
+getClass(2, 20, 10);
+})();
+
+//获取hotClass
+(function() {
+
+})();
 
 
 
 
 
 
-// 仿jQuery调用 userName: "95b9941b277caf1c77ee35fee66fc5f6", password: "a972aec008fd064f00ae77c3a6472cc2"
-// var a = "95b9941b277caf1c77ee35fee66fc5f6";
-// var b = "a972aec008fd064f00ae77c3a6472cc2";
-// console.log(a);
-// ajax({
-//     url: "http://study.163.com/webDev/login.htm", //请求地址
-//     type: "get", //请求方式
-//     data: {
-//         userName: a,
-//         password: b
-//     }, //请求参数
-//     success: function(res, xml) {
-//         console.log(res);
-//     },
-//     fail: function(status) {
-//         console.log(status);
-//     }
-// });
+
+
+
+
+
+
 
 
 
@@ -169,7 +149,8 @@ function _login() {
 
 var page = document.getElementById('pg');
 page.addEventListener('click', function(event) {
-    getClass();
+
+    getClass(1,20,10);
 });
 
 
@@ -194,39 +175,49 @@ page.addEventListener('click', function(event) {
 
 
 // hotClass();
-// function hotClass() {
-//     ajax({
-//         url: "http://study.163.com/webDev/hotcouresByCategory.htm", //请求地址
-//         type: "get", //请求方式
-//         data: {}, //请求参数
-//         success: function(res, xml) {
-//             var resp = JSON.parse(res);
-//             var id = [];
-//             for (var i = 0, l = resp.length; i < l; i++) {
-//                 id.push(resp[i].id);
-//             }
-//             console.log(id[0]);
-//             // return id;
-//         },
-//         fail: function(status) {
-//             console.log('someThing was wrong: ' + status);
-//         }
-//     });
-// }
-
-// // getClass();
-function getClass() {
+function hotClass() {
     ajax({
-        url: "http://study.163.com/webDev/couresByCategory.htm", //请求地址
+        url: "http://study.163.com/webDev/hotcouresByCategory.htm", //请求地址
         type: "get", //请求方式
-        data: { pageNo: "2", psize: "20", type: "20" }, //请求参数
-        // dataType: "json",
+        data: {}, //请求参数
         success: function(res, xml) {
             var resp = JSON.parse(res);
-            console.log(resp);
+            var id = [];
             for (var i = 0, l = resp.length; i < l; i++) {
-                // console.log(resp[i]);
+                id.push(resp[i].id);
             }
+            console.log(id[0]);
+            // return id;
+        },
+        fail: function(status) {
+            console.log('someThing was wrong: ' + status);
+        }
+    });
+}
+
+//获取课程 page当前页数 size每页显示个数 type类型 10、20
+function getClass(page, size, type) {
+    ajax({
+        url: "http://study.163.com/webDev/couresByCategory.htm",
+        type: "get",
+        data: { pageNo: page, psize: size, type: type },
+        success: function(res, xml) {
+            var resp = JSON.parse(res);
+
+            // 生成课程
+            var course = document.getElementById('course');
+            course.innerHTML = '';
+            for (var i = 0; i < size; i++) {
+                var diva = document.createElement('div');
+                diva.innerHTML = '  <i class="img"><img src="'+resp.list[i].middlePhotoUrl+'"></i>\
+                    <h3>' + resp.list[i].name + '</h3>\
+                    <span>' + resp.list[i].provider + '</span>\
+                    <div class="fans"><i></i><span>' + resp.list[i].learnerCount + '</span></div>\
+                    <div class="price">￥' + resp.list[i].price + '</div>';
+                course.appendChild(diva);
+                diva.className = 'cell';
+            }
+
         },
         fail: function(status) {
             console.log('someThing was wrong: ' + status);
@@ -237,18 +228,39 @@ function getClass() {
 
 // // 导航关注
 // // getNotice();
-// function getNotice() {
-//     ajax({
-//         url: "http://study.163.com/webDev/attention.htm", //请求地址
-//         type: "get", //请求方式
-//         data: {}, //请求参数
-//         success: function(res, xml) {
-//             var resp = JSON.parse(res);
-//             // console.log(resp);
-//             return resp;
-//         },
-//         fail: function(status) {
-//             console.log('someThing was wrong: ' + status);
-//         }
-//     });
-// }
+function getNotice() {
+    ajax({
+        url: "http://study.163.com/webDev/attention.htm", //请求地址
+        type: "get", //请求方式
+        data: {}, //请求参数
+        success: function(res, xml) {
+            var resp = JSON.parse(res);
+            // console.log(resp);
+            return resp;
+        },
+        fail: function(status) {
+            console.log('someThing was wrong: ' + status);
+        }
+    });
+}
+
+
+
+// 仿jQuery调用 userName: "95b9941b277caf1c77ee35fee66fc5f6", password: "a972aec008fd064f00ae77c3a6472cc2"
+// var a = "95b9941b277caf1c77ee35fee66fc5f6";
+// var b = "a972aec008fd064f00ae77c3a6472cc2";
+// console.log(a);
+// ajax({
+//     url: "http://study.163.com/webDev/login.htm", //请求地址
+//     type: "get", //请求方式
+//     data: {
+//         userName: a,
+//         password: b
+//     }, //请求参数
+//     success: function(res, xml) {
+//         console.log(res);
+//     },
+//     fail: function(status) {
+//         console.log(status);
+//     }
+// });

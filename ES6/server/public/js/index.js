d7a5d8e2ca02c7b46bfef58ec95ddf7dc0ec1480
Eric Jiang <deviljiang021@gmail.com>
2017-06-07 15:57:24 +0800

    6_7

--- ES6/server/public/js/index.js
+++ ES6/server/public/js/index.js
@@ -59,8 +59,38 @@
 /* 2 */
 /***/ (function(module, exports) {
 
+	'use strict';
+
 	// 正则扩展
-	"use strict";
+	{
+	  var regex = new RegExp('xyz', 'i');
+	  var regex2 = new RegExp(/xyz/i);
+
+	  console.log(regex.test('xyz123'), regex2.test('xyz123'));
+
+	  // 第二个修饰符会覆盖第一个中的修饰符
+	  var regex3 = new RegExp(/xyz/ig, 'i');
+	  console.log(regex3.flags);
+	}
+
+	{
+	  var s = 'qqq_qqq_q';
+	  var a1 = /q+/g;
+	  var a2 = new RegExp('q+', 'y');
+	  // 都是全局匹配 但是g是从上次匹配的位置开始匹配 中间匹配上也算 y 粘连模式是匹配上的下一个紧贴着的下一个
+	  console.log('one', a1.exec(s), a2.exec(s));
+	  console.log('two', a1.exec(s), a2.exec(s));
+	  // 检测是否开启粘连模式
+	  console.log(a1.sticky, a2.sticky);
+	}
+	// 第一个当成2个字符，u会当成一个字符
+	{
+	  console.log('u-1', /^\uD83D/.test('\uD83D\uDC2A'));
+	  console.log('u-2', /^(?:\uD83D(?![\uDC00-\uDFFF]))/.test('\uD83D\uDC2A'));
+	  // 中间放的是编码 要加u才能识别
+	  console.log(/\u{61}/.test('a'));
+	  console.log(/a/.test('a'));
+	}
 
 /***/ })
 /******/ ]);
